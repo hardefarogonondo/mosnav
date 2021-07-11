6bf7a80b1d6b63cf1e612c43cdecabf947ce8dab
hardefarogonondo <hardefarogonondo@gmail.com>
2021-07-11 16:02:11 +0700

    new

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
-from flask import Blueprint, Flask, render_template, abort
-# from jinja2 import TemplateNotFound
-
-# simple_page = Blueprint('simple_page', __name__, template_folder = 'templates')
-
-# @simple_page.route('/', defaults = {'page': 'index'})
-# @simple_page.route('/<page')
-# def show(page):
-#     try:
-#         return render_template('pages/s%.html' % page)
-#     except TemplateNotFound:
-#         abort(404)
+from flask import Flask, Blueprint
+from flask import render_template, request
+import foo_api
 
 app = Flask(__name__)
 app.debug = True
@@ -17,6 +8,18 @@ app.debug = True
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/', methods = ['POST'])
+def search():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
+@app.route('/')
+def get_data():
+    events = api.call(get_event, arg0, arg1)
+    geocode = event['latitude'], event['longitude']
+    return render_template('get_data.html', geocode = geocode)
 
 if __name__ == '__main__':
     app.run()
