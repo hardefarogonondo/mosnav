855da8fe934e4cf99813d0bf0eab8cd2bce895ad
hardefarogonondo <hardefarogonondo@gmail.com>
2021-07-11 16:41:16 +0700

    test api

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, Blueprint
 from flask import render_template, request
-import foo_api
+import requests
+
+key = '47ec0d7d-ea0a-4a20-bcee-66b897ea33bb'
+
+url = "https://geocode-maps.yandex.ru/1."
 
 app = Flask(__name__)
 app.debug = True
@@ -14,6 +18,19 @@ def search():
     text = request.form['text']
     processed_text = text.upper()
     return processed_text
+
+# @app.route('/')    
+# def get_posts():
+#     r = requests.get('https://api-maps.yandex.ru/2.1/?apikey=47ec0d7d-ea0a-4a20-bcee-66b897ea33bb API-ключ&lang=ru_RU')
+#     data = r.text
+#     jsonData = []
+#     for post in data:
+#         r = requests.get('https://api-maps.yandex.ru/2.1/?apikey=47ec0d7d-ea0a-4a20-bcee-66b897ea33bb API-ключ&lang=ru_RU')
+#         r.text
+#         jsonData.append(r.text)
+#     jsonData = jsonify(jsonData)
+#     print jsonData  
+#     return jsonData
 
 @app.route('/')
 def get_data():
